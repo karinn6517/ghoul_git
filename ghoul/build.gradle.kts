72f61be19624d483a96ee29278c94217ea5c71c5
karinn6517 <karinn6517@gmail.com>
2025-09-10 07:16:38 +0000

    環境の調整

--- ghoul/build.gradle.kts
+++ ghoul/build.gradle.kts
@@ -25,13 +25,9 @@ repositories {
 	mavenCentral()
 }
 
-extra["springCloudGcpVersion"] = "7.3.0"
-extra["springCloudVersion"] = "2025.0.0"
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.google.cloud:spring-cloud-gcp-starter")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
@@ -39,13 +35,6 @@ dependencies {
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-dependencyManagement {
-	imports {
-		mavenBom("com.google.cloud:spring-cloud-gcp-dependencies:${property("springCloudGcpVersion")}")
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
 }
 
 tasks.withType<Test> {
